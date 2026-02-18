51a205fbe02afb42dc0297821c47eddab3928a18
Hannah Owen <hannahowen2001@example.com>
2026-02-18 12:29:51 -0500

    Updated all functions

--- main.R
+++ main.R
@@ -128,7 +128,18 @@ affy_to_hgnc <- function(affy_vector) {
 #' `1 202860_at   DENND4B good        7.16      ...`
 #' `2 204340_at   TMEM187 good        6.40      ...`
 reduce_data <- function(expr_tibble, names_ids, good_genes, bad_genes){
-    return(NULL)
+  reduced_data <- expr_tibble %>% 
+    left_join(names_ids, by=c("probe" = "affy_hg_u133_plus_2")) %>% 
+    mutate(
+      gene_set = case_when(
+        hgnc_symbol %in% good_genes ~ "good",
+        hgnc_symbol %in% bad_genes  ~ "bad",
+        TRUE ~ NA_character_)
+      ) %>% 
+    filter(!is.na(gene_set)) %>% 
+    select(probe, hgnc_symbol, gene_set, everything())
+  
+    return(reduced_data)
 }
 
 #' Convert a wide format tibble to long for easy plotting
